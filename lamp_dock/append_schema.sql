a9e0763103ac91b5fdd7d4f3864eaba3bb1ecafc
Asuka Arai <assu0121@gmail.com>
2021-01-31 17:01:40 +0900

    購入履歴用テーブル

---
+++ lamp_dock/append_schema.sql
@@ -0,0 +1,87 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: mysql
+-- 生成日時: 2021 年 1 月 31 日 07:56
+-- サーバのバージョン： 5.7.32
+-- PHP のバージョン: 7.4.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `sample`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `histories`
+--
+
+CREATE TABLE `histories` (
+  `history_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `total_price` int(11) DEFAULT NULL,
+  `purchase_date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `history_details`
+--
+
+CREATE TABLE `history_details` (
+  `detail_id` int(11) NOT NULL,
+  `history_id` int(11) DEFAULT NULL,
+  `item_id` int(11) DEFAULT NULL,
+  `purchase_price` int(11) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `subtotal_price` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `histories`
+--
+ALTER TABLE `histories`
+  ADD PRIMARY KEY (`history_id`);
+
+--
+-- テーブルのインデックス `history_details`
+--
+ALTER TABLE `history_details`
+  ADD PRIMARY KEY (`detail_id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `histories`
+--
+ALTER TABLE `histories`
+  MODIFY `history_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- テーブルの AUTO_INCREMENT `history_details`
+--
+ALTER TABLE `history_details`
+  MODIFY `detail_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
